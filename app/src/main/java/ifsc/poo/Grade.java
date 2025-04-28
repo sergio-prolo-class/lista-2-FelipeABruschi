a1eaad15656c47a32d6b29b4ed70ad91604cfa97
Aluno <FelipeABruschi>
2025-04-28 09:23:45 -0300

    tabuleiro feito

--- app/src/main/java/ifsc/poo/Grade.java
+++ app/src/main/java/ifsc/poo/Grade.java
@@ -5,11 +5,19 @@ import edu.princeton.cs.algs4.Draw;
 public class Grade {
 
     public void desenhar(Draw tabuleiro){
-//        tabuleiro.rectangle(20, 20, 20, 20);
-//        tabuleiro.rectangle(20, 40, 20, 20);
 
-        for(int i = 40; i <= 400; i += 40)
-            for(int j = 40; j <= 400; j += 40)
-                tabuleiro.rectangle(i, j, 20, 20);
+        int numero = 0;
+        char letra = 65;
+
+        for(int i = 60; i <= 420; i += 40, letra++){
+            tabuleiro.text(20, i, Character.toString(letra));
+            for(int j = 60; j <= 420; j += 40, numero++) {
+                if(i == 60)
+                    tabuleiro.text(j, 20, Integer.toString(numero));
+                tabuleiro.square(i, j, 20);
+            }
+        }
+
+
     }
 }
